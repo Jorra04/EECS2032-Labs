c639f936970d0dc11b496365d73a2bc72c9a3f51
jorra04 <jorra04@my.yorku.ca>
2020-01-30 23:56:54 -0500

    added a skeleton of a factorial program, doesnt work yet, needs more work.

--- C practice/factorial.c
+++ C practice/factorial.c
@@ -1,5 +1,24 @@
 #include <stdio.h>
-int main(){
-    printf("Hello, world\n");
-    return 0;
+void main(){
+    printf("Enter the Value.");
+    float val = 0;
+    scanf("%f",&val);
+    if(&val < 0){
+        printf("No factorial for negative numbers.");
+        return -1;
+    }
+    else if(val == 1 || val == 0){
+        return 1;
+    }
+    else{
+        float x = 1;
+        printf("here");
+        for(int i =0; i < val; i++){
+            x*=val;
+        }
+        printf(x);
+        return x;
+    }
+
+    
 }
