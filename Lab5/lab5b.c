85f7dcd64ac7a28a933bf9e7d7f54434425c3477
jorra04 <jorra04@my.yorku.ca>
2020-02-09 01:06:40 -0500

    finished lab5b

--- Lab5/lab5b.c
+++ Lab5/lab5b.c
@@ -1,28 +1,37 @@
 #include <stdio.h>
+#define MAX_SIZE 30
+
+int helper(char s[], int val){
+    int count = 1;
+		for(int i =1; i < MAX_SIZE; i++) {
+			if((s[i] == s[i-1] +1) || (s[i] == s[i-1] -31)  ||  (s[i] == s[i-1] +33)) {
+				count++;
+				if(count == val) {
+					return 1;
+				}
+			}
+		}
+		return 0;
+}
 
 void main(){
     int foundConsec = 1;
     int inConsec;
     char input [30]; 
+    int helperRet;
     fgets(input,sizeof(input), stdin);
     scanf("%d",&inConsec);
-    
-    for(int i =0; i < 30; i ++){
-        if(input[i] != 10){ //lf handling.
-            if(input[i] == input[i+1] -1  || input[i] == input[i+1] -33 || input[i] == input[i+1] +31 ){
-                foundConsec ++;
-                if(foundConsec == inConsec){
-                    printf("YES\n");
-                    break;
-                }
-        }
-        else{
-            foundConsec=1;
-        }
-        }
-        else{
-            printf("NO\n");
-            break;
-        }
+
+    helperRet = helper(input,inConsec);
+
+    if(helperRet == 1){
+        printf("YES\n");
     }
+    else{
+        printf("NO\n");
+    }
+
+    
 }
+
+
