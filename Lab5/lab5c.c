2905bc6767989c5ced947acbe6add755f34ff848
jorra04 <jorra04@my.yorku.ca>
2020-02-09 14:33:30 -0500

    Fixed lab5

--- Lab5/lab5c.c
+++ Lab5/lab5c.c
@@ -1,24 +1,20 @@
 #include <stdio.h>
 
-int getEndingIndex(char str[], int n, int i) 
-    { 
-        i++; 
-        while (i < n)  
-        { 
-            char curr = str[i]; 
-            char prev = str[i-1]; 
-  
-            // If the current character appears after 
-            // the previous character according to  
-            // the given circular alphabetical order 
-            if ((curr == 'a' && prev == 'z') || 
-                (curr - prev == 1)) 
-                i++; 
-            else
-                break; 
+int getEndingIndex(char str[], int n, int i) { 
+    i++; 
+    while (i < n){ 
+        char curr = str[i]; 
+        char prev = str[i-1]; 
+        if ((curr - prev == 1)){
+             i++; 
         } 
+        else{
+            break;
+        }
+                 
+    } 
   
-        return i - 1; 
+    return i - 1; 
     } 
     int max(int num1, int num2){
         if(num1>= num2){
@@ -27,27 +23,19 @@ int getEndingIndex(char str[], int n, int i)
         return num2;
 
     }
-    int largestSubStr(char str[], int n) 
-    { 
+    int largestSubStr(char str[], int n) { 
         int len = 0; 
   
         int i = 0; 
-        while (i < n)  
-        { 
-  
-            // Valid sub-string exists from index 
-            // i to end 
+        while (i < n){ 
             int end = getEndingIndex(str, n, i); 
-  
-            // Update the length 
             len = max(end - i + 1, len); 
             i = end + 1; 
         } 
   
         return len; 
     } 
-    int main() 
-    { 
+    int main() { 
     int foundConsec = 1;
     int inConsec;
     char input [30]; 
