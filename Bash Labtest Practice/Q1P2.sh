1a30cb1544372ecc9f30153d99c6dbf65760e983
jorra04 <jorra04@my.yorku.ca>
2020-02-13 11:05:44 -0500

    Cleaned up Q1P2.sh

--- Bash Labtest Practice/Q1P2.sh
+++ Bash Labtest Practice/Q1P2.sh
@@ -1,15 +1,12 @@
 #!/bin/bash
-
 count=0
 while read ID name
 do
-    #echo "${1// /} = ${ID// /}"
     if [ "${1// /}" = "${ID// /}" ]
     then
         storedName=$name
         while read ID2 name
         do
-            #echo "$name --> $storedName"
             if [ "$name" = "$storedName" ]
             then
                 count=$((count+1))
