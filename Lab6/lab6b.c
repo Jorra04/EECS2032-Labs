180a8e087689f9f2bbc5004ffeb4c1efd5a71608
jorra04 <jorra04@my.yorku.ca>
2020-03-03 19:03:27 -0500

    added lab6.c, completed a possible other method of l6b.c

--- Lab6/lab6b.c
+++ Lab6/lab6b.c
@@ -1,14 +1,7 @@
 #include <stdio.h>
 #include <string.h>
-#define MAX_ARR_SIZE 24
-//UPDATED VERSION OF LAB 6 DOES NOT CHECK THE REVERSE CASE.
-int * zeroMaker(int arr []){
-    int i;
-    for(i = 0; i < MAX_ARR_SIZE; i ++){
-        arr[i] = 0;
-    }
-    return arr;
-}
+#define MAX_ARR_SIZE 35
+
 int maxOfArr(int arr []){
     int k;
     int max = arr[0];
@@ -20,56 +13,38 @@ int maxOfArr(int arr []){
     return max;
 }
 
-int minSize(int s1, int s2){
-    if(s1 >= s2){
-        return s2;
-    }
-    return s1;
-}
-
 int finder(char str1 [], char str2[],int a []){
-    int i;
-    int j;
-    int count = 0;
-    int index = 0;
-    int condition = minSize(strlen(str1)-1,strlen(str2)-1);
+    int i,j,count,index = 0;
+    int condition = (strlen(str1)-1 > strlen(str2)-1) ? strlen(str2)-1 : strlen(str1)-1;
     for(i = 0; i < condition; i++){
         int k = i;
         j = 0;
 
-        
-        while(k >= 0){
-            // printf("%c ---->%c\n",str1[strlen(str1)-k-2],str2[j]);  //print the matching values.
+        while(k >= 0){ // printf("%c ---->%c\n",str1[strlen(str1)-k-2],str2[j]);  //print the matching values.
             if(str1[strlen(str1)-k-2] == str2[j]){
-                count++;
-                
+                count++; 
             }
             else{
-                count = 0;
                 break;
             }
-            
             k--;
             j++;
-           
         }
         a[index] = count; //having this happen outside the loop means that only consecutive counts are added.
         index++;
-        // printf("------------\n"); // lets me see where each iteration begins.
+        count = 0;
     }
-    // printf("%d\n",maxOfArr(a));
     return maxOfArr(a);
 }
 
 void main(){
-    char firstStr [MAX_ARR_SIZE];
-    char secondStr [MAX_ARR_SIZE];
-    int maxFinds [MAX_ARR_SIZE];
-    int maxFinds2 [MAX_ARR_SIZE];
-    zeroMaker(maxFinds2);
-    zeroMaker(maxFinds);
+    char firstStr [MAX_ARR_SIZE]; 
+    char secondStr [MAX_ARR_SIZE]; 
+    int maxFinds [MAX_ARR_SIZE] = {0}; 
+    int maxFinds2 [MAX_ARR_SIZE] = {0};
     fgets(firstStr,sizeof(firstStr),stdin);
     fgets(secondStr,sizeof(secondStr),stdin);
-    int first = finder(firstStr,secondStr,maxFinds);
-    printf("%d\n",first);  
+    int first = finder(firstStr,secondStr,maxFinds); // can print these to show both steps.
+    int second = finder(secondStr,firstStr,maxFinds2);
+    printf("%d\n",(first>second) ? first : second); //gets max return of firstStr into secondStr vs secondStr into firstStr.
 }
